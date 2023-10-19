73fc08fe66d1d5cf6cb6243d5b1702d5f46a2fbc
Sharif Obaidullah <sharifmdobaidullah@gmail.com>
2023-10-20 03:52:30 +0600

    get data for update product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5005;
 
@@ -40,7 +40,12 @@ async function run() {
       res.send(result)
     })
 
-
+    // app.get('/cars/:id',async(req,res)=>{
+    //   const id = req.params.id
+    //   const query = {_id:new ObjectId(id)}
+    //   const result = await carsCollection.findOne(query)
+    //   res.send(result)
+    // })
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
